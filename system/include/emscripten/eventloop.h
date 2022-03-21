f28ef0afd6993f0f57bbc658387d281b330da918
Jukka Jylänki <jujjyl@gmail.com>
2022-03-21 14:50:14 +0200

    Add use of Clang  __attribute__((nonnull)) in Emscripten system headers.

--- system/include/emscripten/eventloop.h
+++ system/include/emscripten/eventloop.h
@@ -15,15 +15,15 @@ extern "C" {
 
 void emscripten_unwind_to_js_event_loop(void) __attribute__((noreturn));
 
-long emscripten_set_timeout(void (*cb)(void *userData), double msecs, void *userData);
+long emscripten_set_timeout(void (*cb)(void *userData) __attribute__((nonnull)), double msecs, void *userData);
 void emscripten_clear_timeout(long setTimeoutId);
 void emscripten_set_timeout_loop(EM_BOOL (*cb)(double time, void *userData), double intervalMsecs, void *userData);
 
-long emscripten_set_immediate(void (*cb)(void *userData), void *userData);
+long emscripten_set_immediate(void (*cb)(void *userData) __attribute__((nonnull)), void *userData);
 void emscripten_clear_immediate(long setImmediateId);
 void emscripten_set_immediate_loop(EM_BOOL (*cb)(void *userData), void *userData);
 
-long emscripten_set_interval(void (*cb)(void *userData), double intervalMsecs, void *userData);
+long emscripten_set_interval(void (*cb)(void *userData) __attribute__((nonnull)), double intervalMsecs, void *userData);
 void emscripten_clear_interval(long setIntervalId);
 
 #ifdef __cplusplus
--- system/include/emscripten/wasmfs.h
+++ system/include/emscripten/wasmfs.h
@@ -17,7 +17,7 @@ extern "C" {
 typedef struct Backend* backend_t;
 
 // Obtains the backend_t of a specified path.
-backend_t wasmfs_get_backend_by_path(char* path);
+backend_t wasmfs_get_backend_by_path(char* path __attribute__((nonnull)));
 
 // Obtains the backend_t of a specified fd.
 backend_t wasmfs_get_backend_by_fd(int fd);
@@ -26,11 +26,11 @@ backend_t wasmfs_get_backend_by_fd(int fd);
 // Returns the file descriptor for the new file like `open`. Returns a negative
 // value on error. TODO: It might be worth returning a more specialized type
 // like __wasi_fd_t here.
-int wasmfs_create_file(const char* pathname, mode_t mode, backend_t backend);
+int wasmfs_create_file(const char* pathname __attribute__((nonnull)), mode_t mode, backend_t backend);
 
 // Creates a new directory in the new file system under a specific backend.
 // Returns 0 on success like `mkdir`, or a negative value on error.
-int wasmfs_create_directory(const char* path, long mode, backend_t backend);
+int wasmfs_create_directory(const char* path __attribute__((nonnull)), long mode, backend_t backend);
 
 // Backend creation
 
@@ -44,9 +44,9 @@ typedef backend_t (*backend_constructor_t)(void*);
 backend_t wasmfs_create_proxied_backend(backend_constructor_t create_backend,
                                         void* arg);
 
-backend_t wasmfs_create_fetch_backend(const char* base_url);
+backend_t wasmfs_create_fetch_backend(const char* base_url __attribute__((nonnull)));
 
-backend_t wasmfs_create_node_backend(const char* root);
+backend_t wasmfs_create_node_backend(const char* root __attribute__((nonnull)));
 
 #ifdef __cplusplus
 }
